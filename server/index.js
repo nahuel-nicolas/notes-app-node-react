ae004554f4ad75a21acc31b7f460559c99406dcf
nahuel-nicolas <nnahuel012@gmail.com>
2022-10-13 11:28:39 -0300

    Finished building express-react

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const NoteModel = require("./models/Notes");
-
 const cors = require("cors");
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -11,28 +11,6 @@ app.use(cors());
 mongoose.connect(
     'mongodb://localhost:27017/mongoprojects'
 );
-
-// mongoose.connect(
-//   "mongodb+srv://user123:Password123Tech@cluster0.j7fql.mongodb.net/merntutorial?retryWrites=true&w=majority"
-// );
-
-// app.get("/getUsers", (req, res) => {
-//   UserModel.find({}, (err, result) => {
-//     if (err) {
-//       res.json(err);
-//     } else {
-//       res.json(result);
-//     }
-//   });
-// });
-
-// app.post("/createUser", async (req, res) => {
-//   const user = req.body;
-//   const newUser = new UserModel(user);
-//   await newUser.save();
-
-//   res.json(user);
-// });
 
 app.get("/notes/", (req, res) => {
   NoteModel.find({}, (err, result) => {
@@ -44,7 +22,7 @@ app.get("/notes/", (req, res) => {
   });
 });
 
-app.get("/notes/:note_id", (req, res) => {
+app.get("/notes/:note_id/", (req, res) => {
   NoteModel.findById(req.params.note_id, (err, result) => {
     if (err) {
       res.json(err);
@@ -53,7 +31,6 @@ app.get("/notes/:note_id", (req, res) => {
     }
   });
 });
-
 
 app.post("/notes/", async (req, res) => {
     // expected req.body = {body:String}
@@ -64,7 +41,7 @@ app.post("/notes/", async (req, res) => {
     res.json(note);
 });
 
-app.put("/notes/:note_id", async (req, res) => {
+app.put("/notes/:note_id/", async (req, res) => {
   // expected req.body = {body:String}
   const note = req.body;
   note.updated = get_current_isodatetimestring()
@@ -77,7 +54,7 @@ app.put("/notes/:note_id", async (req, res) => {
   })
 });
 
-app.delete("/notes/:note_id", (req, res) => {
+app.delete("/notes/:note_id/", (req, res) => {
   NoteModel.findByIdAndDelete(req.params.note_id, (err, result) => {
     if (err) {
       res.json(err);
@@ -87,32 +64,11 @@ app.delete("/notes/:note_id", (req, res) => {
   });
 });
 
-app.get("/try/waw", (req, res) => {
-    res.json({
-        'hello': 'paula FUCKING ASSHOLE'
-    })
-});
-
-app.get("/notes", (req, res) => {
-    res.json({
-        'hello': 'nahuel'
-    })
-});
-
 app.listen(3001, () => {
-  console.log("SERVER RUNS PERFECTLY!");
+  console.log("Node server is running!");
 });
 
 function get_current_isodatetimestring() {
   const currentdate = new Date();
   return currentdate.toISOString();
 }
-
-// var currentdate = new Date(); 
-// var current_datetimestring = currentdate.getDate() + "-"
-//                 + (currentdate.getMonth()+1)  + "-" 
-//                 + currentdate.getFullYear() + " "  
-//                 + currentdate.getHours() + ":"  
-//                 + currentdate.getMinutes() + ":" 
-//                 + currentdate.getSeconds();
-// var current_isodatetime = currentdate.toISOString();
